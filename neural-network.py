600fef46090a66daf73ddd16594b9c7eaf823b5c
Karl Eisen Yabut <karleisenyabut@gmail.com>
2020-07-21 17:50:03 -0400

    finished neural network

--- neural-network.py
+++ neural-network.py
@@ -4,27 +4,43 @@ import numpy as np
 def sigmoid(x):
   return 1 / (1 + np.exp(-x))
 
+def sigmoid_derivative(x):
+  return x * (1 - x)
+
 training_inputs = np.array([[0,0,1],
-                           [1,1,1],
-                           [1,0,1],
-                           [0,1,1]])
+                            [1,1,1],
+                            [1,0,1],
+                            [0,1,1]])
 
 training_outputs = np.array([[0,1,1,0]]).T
 
 np.random.seed(1)
 
 #create 3x1 because we have 3 input and 1 output, random values from -1 to 1
-synaptic_weights = 2 * np.random.random((3, 1)) - 1
+synaptic_weights = 2 * np.random.random((3,1)) - 1
 
 print('Random starting synaptic weights: ')
 print(synaptic_weights)
 
 
-for iteration in range(1):
+for iteration in range(20000):
   
   input_layer = training_inputs
 
   outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+
+  #calculate the error
+  error = training_outputs - outputs
+
+  #adjust the weights
+  adjustments = error * sigmoid_derivative(outputs)
+
+  #update the weights
+  synaptic_weights += np.dot(input_layer.T, adjustments)
+
+
+print('Synaptic weights after training: ')
+print(synaptic_weights)
 
 print('Outputs after training: ')
 print(outputs)
